4b3ae7276ef6c3fcb8cb052832fe1dc7f8c8af6b
LukaV <lvlahovic16@raf.rs>
2026-03-21 20:10:55 +0100

    Added CRUP operations for Station

---
+++ Dtos/StationDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RailwayManagementSystemAPI.Dtos
+{
+    public class StationDto
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string City { get; set; } = string.Empty;
+
+        [StringLength(100)]
+        public string Country { get; set; } = string.Empty;
+
+        [Range(0, 20)]
+        public int NumberOfPlatforms { get; set; }
+    }
+}
